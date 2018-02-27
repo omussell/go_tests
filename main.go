1a90f09de3b9f4dc6519f4aeb92c3f6b93cd42b5
Oliver <oliver.mussell@hotmail.co.uk>
2018-02-27 19:43:57 +0000

    Update to print just name for now

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
-//import "github.com/brianvoe/gofakeit"
+import "github.com/brianvoe/gofakeit"
 import "fmt"
 
 func main() {
-//	gofakeit.Seed(0)
-//	fmt.Println(gofakeit.Name())
-	fmt.Println("test")
+	gofakeit.Seed(0)
+	fmt.Println(gofakeit.Name())
+	//	fmt.Println("test")
 }
