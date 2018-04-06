bdf5bf45d38ef831fd99135e503d24bf64caf555
omussell <oliver.mussell@primarysite.net>
2018-04-06 15:07:03 +0100

    Fix layout of CI file

--- buildbot/buildbot.py
+++ buildbot/buildbot.py
@@ -1,15 +1,14 @@
-c['schedulers'].append(schedulers.SingleBranchScheduler(
-                            name="testing factory",
-                            change_filter=util.ChangeFilter(branch='master'),
-                            treeStableTimer=None,
-                            #fileIsImportant=config_updated,
-                            builderNames=["testingbuilder"]))
+testfactory = util.BuildFactory()
+testfactory.addStep(steps.ShellCommand(command=["echo", "build"],))
 
 c['builders'].append(
     util.BuilderConfig(name="testingbuilder",
       workernames=["buildmaster"],
       factory=testfactory))
 
-testfactory = util.BuildFactory()
-testfactory.addStep(steps.ShellCommand(command=["echo", "build"],))
-
+c['schedulers'].append(schedulers.SingleBranchScheduler(
+                            name="testing factory",
+                            change_filter=util.ChangeFilter(branch='master'),
+                            treeStableTimer=None,
+                            #fileIsImportant=config_updated,
+                            builderNames=["testingbuilder"]))
