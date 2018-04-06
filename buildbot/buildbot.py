e518564bd361e80e3995f99894a945425f22e5b9
omussell <oliver.mussell@primarysite.net>
2018-04-06 12:38:35 +0100

    Fix ordering

--- buildbot/buildbot.py
+++ buildbot/buildbot.py
@@ -1,7 +1,8 @@
-testfactory = util.BuildFactory()
-testfactory.addStep(steps.ShellCommand(command=["echo", "build"],))
-
 c['builders'].append(
     util.BuilderConfig(name="test",
       workernames=["buildmaster"],
       factory=testfactory))
+
+testfactory = util.BuildFactory()
+testfactory.addStep(steps.ShellCommand(command=["echo", "build"],))
+
