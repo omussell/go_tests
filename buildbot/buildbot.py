bcdbcfbec23fce90247efa5b7b3ee0e0ccc634d8
omussell <oliver.mussell@primarysite.net>
2018-04-06 12:37:16 +0100

    Add basic builder

--- buildbot/buildbot.py
+++ buildbot/buildbot.py
@@ -1,2 +1,7 @@
 testfactory = util.BuildFactory()
 testfactory.addStep(steps.ShellCommand(command=["echo", "build"],))
+
+c['builders'].append(
+    util.BuilderConfig(name="test",
+      workernames=["buildmaster"],
+      factory=testfactory))
