3c2776e4afd6facbfc00dadeaba4d1ca1675f459
omussell <oliver.mussell@primarysite.net>
2018-04-06 12:44:25 +0100

    Add scheduler

--- buildbot/buildbot.py
+++ buildbot/buildbot.py
@@ -1,5 +1,12 @@
+c['schedulers'].append(schedulers.SingleBranchScheduler(
+                            name="testing factory",
+                            change_filter=util.ChangeFilter(branch='master'),
+                            treeStableTimer=None,
+                            #fileIsImportant=config_updated,
+                            builderNames=["testingbuilder"]))
+
 c['builders'].append(
-    util.BuilderConfig(name="test",
+    util.BuilderConfig(name="testingbuilder",
       workernames=["buildmaster"],
       factory=testfactory))
 
